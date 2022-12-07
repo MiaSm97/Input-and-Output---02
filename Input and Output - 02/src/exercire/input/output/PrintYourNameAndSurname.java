60f926dc58fa28f9f84a2743dab28919cdc5aa35
MiaSm97 <118892985+MiaSm97@users.noreply.github.com>
2022-12-07 11:20:34 +0100

    Esercizio #18

---
+++ Input and Output - 02/src/exercire/input/output/PrintYourNameAndSurname.java
@@ -0,0 +1,14 @@
+package exercire.input.output;
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+        System.out.print("Mia ");
+        System.out.print("Smaldone");
+    }
+}
+
+    /*create a class called PrintYourNameAndSurname
+        provide the class PrintYourNameAndSurname with a main method:
+public static void main(String[] args) { ... }
+        inside the main method write two lines of code that print on the same line your name followed by a space and by your surname
+        the expected output should be this:
+        YourName YourSurname*/
